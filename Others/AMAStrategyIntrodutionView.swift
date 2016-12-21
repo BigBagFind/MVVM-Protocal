083b5b44c52e95c344edffc1cd8769f8ebb9755b
wuyt <594228924@qq.com>
2016-12-22 00:56:22 +0800

    增加了策略介绍的头视图

---
+++ Others/AMAStrategyIntrodutionView.swift
@@ -0,0 +1,19 @@
+//
+//  AMAStrategyIntrodutionView.swift
+//  PtpurExtensionDemo
+//
+//  Created by 吴玉铁 on 2016/12/22.
+//  Copyright © 2016年 wuyutie. All rights reserved.
+//
+
+import UIKit
+
+class AMAStrategyIntrodutionView: UIView {
+
+    @IBOutlet weak var moreDetailButton: UIButton!  // 更多说明
+   
+    @IBOutlet weak var profitRateLabel: UILabel!    // 策略回报率
+    
+    
+    
+}
